413c09016092fabd19e473a94db44c8ec10b1600
aviyot <40815372+aviyot@users.noreply.github.com>
2020-01-30 02:24:24 +0200

    cont....

--- src/components/CalcTime.vue
+++ src/components/CalcTime.vue
@@ -1,35 +1,86 @@
 <template>
-  <div>
+  <div class="calc-app">
     <h1>Calc</h1>
-   <span>Quntity Rer Hour</span> <input v-model="z" /><br/>
-    <span>Intial Quntity </span> <input v-model="x"><br/>
-    <span>Total Quntity </span><input v-model="y" /><br/>
-<div>
-<span>Left Quntity: {{y-x}}</span>
-</div>
-<span>Time To Finish:</span>{{((y-(x-1))/(z/60))}}
-  </div>
+    <span>Speed(RPH) :</span>
+    <input v-model="rph" maxlength="8">
+    <br>
+    <span>Total :</span>
+    <input v-model="total">
+    <br>
+    <span>Current:</span>
+    <input v-model="current">
+    <div>
+      <span>from :</span>
+      <input type="number" v-model="from">
+      <span>
+        Until :
+        <input type="number" v-model="until">
+      </span>
+    </div>
+    <br>
+    <br>
+    <span>Time Total:</span>
+    {{(total/(rph/minute))}}min
+    <br>
+    <br>
 
+    <span>Left Quntity: {{total-current}}</span>
+    <br>
+
+    <span>Left Time :</span>
+    {{((current)/(rph/minute))}}min
+    <br>
+    <br>
+
+    <span>Quntity Until Current:</span>
+    {{total-current}}
+    <br>
+    <span>Time Until Current:</span>
+    {{((total-current)/(rph/minute))}}min
+    <br>
+    <br>
+
+    <span>Quntity from - until:</span>
+    {{until - from}}
+    <br>
+    <span>Time from - until:</span>
+    {{((until - from)/(rph/minute))}}min
+  </div>
 </template>
 
 <script>
 export default {
   name: "CalcTime",
   props: {},
-  data : ()=> ({
-    x:"1",
-    y:500,
-    z:10000,
-    res:""
+  data: () => ({
+    total: 10000,
+    rph: 10000,
+    from: 500,
+    until: 700,
+    current: 5000,
+    intialRPR: 10000,
+    minute: 60
   }),
-  methods : {
-    calc : ()=>{
-     
-    }
+  methods: {
+    calc: () => {}
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input {
+  display: inline-block;
+  width: 60px;
+  background-color: blue;
+  color: white;
+}
+.calc-app {
+  font-size: large;
+  background-color: lightgray;
+  width: 80%;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
 </style>
