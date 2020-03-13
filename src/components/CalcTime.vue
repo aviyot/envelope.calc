6f0143994d47c8a4f9afba0f2149eb0fef76ff1c
aviyot <40815372+aviyot@users.noreply.github.com>
2020-03-13 09:45:02 +0200

    style time component

--- src/components/CalcTime.vue
+++ src/components/CalcTime.vue
@@ -2,8 +2,6 @@
   <div class="calc-app">
     <!-- <h1 style="text-align:center">Calc</h1> -->
     <div class="data">
-      <label>Speed(RPH)</label>
-      <input v-model="rph" type="number">
       <label>From</label>
       <input type="number" v-model="from">
       <label>Until</label>
@@ -12,9 +10,15 @@
     <div>
       <span>Amount : {{from-until + 1}}</span>
       <br>
-      <span>Duration Time : {{(calcTime(from,until,rph,minute))}}</span>
+     <div class="time">
+      <br/>
+      <div class="time-title">TIME</div>
+      <label>Speed(RPH)</label>
+      <input v-model="rph" type="number">
+      <span>Duration : {{(calcTime(from,until,rph,minute))}}</span>
       <br>
-      <span>End Time : {{endTime}}</span>
+      <span>End : {{endTime}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -43,7 +47,7 @@ export default {
         .add(result, "minutes")
         .format("HH:mm:ss");
     //  return `${dr.asHours()}h:${dr.asMinutes()}m`;
-    return dr.format("hh:mm")
+    return dr.format("hh:mm:ss")
     }
   }
 };
@@ -55,5 +59,17 @@ export default {
 .data {
   display: grid;
   grid-template-columns: 2fr 1fr;
+}
+.time input {
+  max-width: 20px;
+}
+
+.time {
+  background-color: blue;
+  color: white;
+}
+
+.time-title{
+text-align: center;
 }
 </style>
