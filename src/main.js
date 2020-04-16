c3d588b96a391b36a092e5d8f797159faf499689
aviyot <aviyot@gmail.com>
2020-04-16 17:14:03 +0300

    add pwa by vue cli

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
  import Vue from "vue";
-import App from "./App.vue";
-import 'bootstrap/dist/css/bootstrap.min.css'
+ import App from "./App.vue";
+ import 'bootstrap/dist/css/bootstrap.min.css'
+ import './registerServiceWorker'
 
-Vue.config.productionTip = false;
+ Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+ new Vue({
+   render: h => h(App)
+ }).$mount("#app");
